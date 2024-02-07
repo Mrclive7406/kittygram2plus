01d4c03f58b029ed4fd7004eaed315826271011e
Pavel KOlesnikov <practYaPasha@yandex.ru>
2024-02-07 12:47:57 +0300

    1

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.throttling import ScopedRateThrottle
+from .throttling import WorkingHoursRateThrottle
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
+from .permissions import OwnerOrReadOnly, ReadOnly
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
@@ -8,9 +13,30 @@ from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle,)
+    throttle_scope = 'low_request'
+    pagination_class = None
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                    filters.OrderingFilter)
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('achievements__name', 'owner__username')
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
+    
+    def get_queryset(self):
+        queryset = Cat.objects.all()
+        color = self.kwargs['color']
+        queryset = queryset.filter(color=color)
+        return queryset 
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions() 
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'cats.apps.CatsConfig',
+    'django_filters', 
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,22 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        #'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.ScopedRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10000/day',  # Лимит для UserRateThrottle
+        'anon': '1000/day',  # Лимит для AnonRateThrottle
+        'low_request': '1/minute',
+    }
+}
+{
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated', 
     ],
